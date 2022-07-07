99c132b8730034d6ab3f2da562372e9995125c55
blueteamhunter <matheus.f.o@hotmail.com>
2022-07-07 15:34:11 -0300

    DNS Transfer Zone Check Implemented

--- sheriff.py
+++ sheriff.py
@@ -1,4 +1,5 @@
 import dns.resolver
+import dns.zone
 import sys
 
 def resolver_query_a(name, query):
@@ -11,18 +12,35 @@ def resolver_query_a(name, query):
     except:
         print("{} Record Lookup not found".format(query))
         pass
+    
+def dns_zone_transfer(address):
+    print ("\n[*] Atempting zone transfer [*]\n")
+    dns_ns_awnswer = dns.resolver.resolve(address, 'NS')
+    for server in dns_ns_awnswer:
+        dns_ns_awnswer = dns.resolver.resolve(address, 'A')
+        for ip in dns_ns_awnswer:
+            try:
+                zone = dns.zone.from_xrf(dns.query.xfr(str(ip), address))
+                for host in zone:
+                    print("[*] Possible zone transfer found: {}".format(host))
+            except Exception as e:
+                print("[*] NS {} is refusing zone transfer!".format(server))
+                continue
 
 def main(argv):
     if len (sys.argv) != 2:
         print("Usage: python example.com")
         sys.exit(1)
     dns_name = str(sys.argv[1])
+    dns_name = "google.com"
     dns_zone = ""
     dns_all_zones = ["A", "MX", "NS", "SOA", "TXT"]
 
     for item in dns_all_zones:
         dns_zone = item
         resolver_query_a(dns_name, dns_zone)
+        
+    dns_zone_transfer(dns_name)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
